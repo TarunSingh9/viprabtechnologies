4c1887fb4a64afeb4342c829108a7d7a56fcb92c
TarunSingh9 <tarundeadend3@gmail.com>
2024-08-18 11:17:57 +0530

    Update repository data with new changes

--- src/Navbar/navbar.jsx
+++ src/Navbar/navbar.jsx
@@ -1,62 +1,42 @@
 import React, { useState } from 'react';
 import "./navbar.css";
-import { FaHome, FaInfoCircle, FaServicestack, FaPhone, FaStar } from 'react-icons/fa';
+import { FaHome, FaInfoCircle, FaServicestack, FaPhone, FaBars, FaTimes } from 'react-icons/fa';
 
 const Navbar = () => {
   const [activeLink, setActiveLink] = useState('#home');
+  const [menuOpen, setMenuOpen] = useState(false);
 
   const handleClick = (link) => {
     setActiveLink(link);
+    setMenuOpen(false);  // Close the menu when a link is clicked
   };
 
   return (
     <nav>
-      <a href="#" className='title'>
-        Viprab Technologies
-      </a>
-      <ul>
+      <div className='title'>Viprab Technologies</div>
+      {!menuOpen ? (
+        <FaBars className="menu-icon" onClick={() => setMenuOpen(true)} />
+      ) : (
+        <FaTimes className="menu-close" onClick={() => setMenuOpen(false)} />
+      )}
+      <ul className={menuOpen ? 'nav-links-mobile' : 'nav-links'}>
         <li>
-          <a
-            href="#home"
-            className={activeLink === '#home' ? 'active' : ''}
-            onClick={() => handleClick('#home')}
-          >
+          <a href="#home" className={activeLink === '#home' ? 'active' : ''} onClick={() => handleClick('#home')}>
             <FaHome /> Home
           </a>
         </li>
         <li>
-          <a
-            href="#about"
-            className={activeLink === '#about' ? 'active' : ''}
-            onClick={() => handleClick('#about')}
-          >
+          <a href="#about" className={activeLink === '#about' ? 'active' : ''} onClick={() => handleClick('#about')}>
             <FaInfoCircle /> About
           </a>
         </li>
         <li>
-          <a
-            href="#services"
-            className={activeLink === '#services' ? 'active' : ''}
-            onClick={() => handleClick('#services')}
-          >
+          <a href="#services" className={activeLink === '#services' ? 'active' : ''} onClick={() => handleClick('#services')}>
             <FaServicestack /> Services
           </a>
         </li>
         <li>
-          <a
-            href="#review"
-            className={activeLink === '#review' ? 'active' : ''}
-            onClick={() => handleClick('#review')}
-          >
-            <FaStar /> Review
-          </a>
-        </li>
-        <li>
-          <a
-            href="#contact"
-            className={activeLink === '#contact' ? 'active' : ''}
-            onClick={() => handleClick('#contact')}
-          >
+          <a href="#contact" className={activeLink === '#contact' ? 'active' : ''} onClick={() => handleClick('#contact')}>
             <FaPhone /> Contact
           </a>
         </li>
--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -13,17 +13,17 @@ const about = () => {
           <img src="https://equitablegrowth.org/wp-content/uploads/2021/12/Man-working-with-machines.jpg" alt="Automation Future Impact" />
         </div>
         <div className="description">
-          <h2>Future Impact</h2>
+          <h2 className='sub-heading'>Future Impact</h2>
           <p>Automation is significantly transforming industries by enhancing productivity and efficiency. It allows for more agile production systems, where processes can be rapidly adapted to changes in demand without stopping the line to reconfigure settings. Innovations in AI and sensor technologies enable robots to perform tasks with a high degree of variability and precision, opening up opportunities for automation in areas like agriculture and intricate electronic assembly.</p>
         </div>
       </section>
-      
+       
       <section id="goals">
         <div className="image">
           <img src="https://importanceoftechnology.net/wp-content/uploads/2021/01/Importance-Of-Technology-In-Oil-And-Gas-Industry.jpg" alt="Automation Goals" />
         </div>
         <div className="description">
-          <h2>Goals</h2>
+          <h2 className='sub-heading'>Goals</h2>
           <p>The primary goal of industrial automation is to increase operational efficiency and reduce costs. By integrating advanced technologies like the Industrial Internet of Things (IIoT) and AI, factories are becoming smarter. This includes the adoption of predictive maintenance, which minimizes downtime, and seamless interoperability among different systems, facilitating smoother operations and data-driven decision making.</p>
         </div>
       </section>
@@ -33,7 +33,7 @@ const about = () => {
           <img src="https://img.freepik.com/free-photo/photo-automobile-production-line-welding-car-body-modern-car-assembly-plant-auto-industry-male-worker-orange-protective-helmet_645730-108.jpg" alt="Automation Needs" />
         </div>
         <div className="description">
-          <h2>Needs</h2>
+          <h2 className='sub-heading'>Needs</h2>
           <p>To keep pace with the rapid advancements in technology, industries need to focus on upskilling their workforce to handle new tools and technologies. This involves training in digital skills, AI, and the management of automated systems. Additionally, industries need to invest in state-of-the-art infrastructure and ensure their systems are compatible with the latest standards in connectivity and data exchange.</p>
         </div>
       </section>
@@ -43,7 +43,7 @@ const about = () => {
           <img src="https://www.ashlingpartners.com/wp-content/uploads/2023/07/Blog-42-The-Role-of-Automation-in-the-Future-of-Energy-for-Oil-Gas-scaled-e1700084190993.jpg" alt="Automation Importance" />
         </div>
         <div className="description">
-          <h2>Importance</h2>
+          <h2 className='sub-heading'>Importance</h2>
           <p>Industrial automation is crucial for sustaining competitive advantage in a global market. It allows businesses to maintain high quality and consistency in production, manage labor costs, and improve safety standards. Moreover, automation is integral in meeting the growing demands for sustainability and efficiency, helping companies to adhere to environmental regulations and optimize resource use.</p>
         </div>
       </section>
@@ -53,7 +53,7 @@ const about = () => {
           <img src="https://www.electricsolenoidvalves.com/product_images/uploaded_images/oil-field-upstream.jpg" alt="Challenges and Opportunities" />
         </div>
         <div className="description">
-          <h2>Challenges</h2>
+          <h2 className='sub-heading'>Challenges</h2>
           <p>While automation presents numerous opportunities, it also poses challenges, particularly in terms of workforce displacement. However, history suggests that technology often creates more jobs than it displaces, and the jobs that are affected are transformed rather than eliminated. New roles that are emerging are often more focused on technology management, oversight, and innovation.</p>
         </div>
       </section>
